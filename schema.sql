0fe7cc55aa99667662480203ab14ed898b40ec3b
mikeheiser9 <mikeheiser9@gmail.com>
2019-09-30 01:44:53 -0400

    first commit

---
+++ schema.sql
@@ -0,0 +1,12 @@
+-- CREATE DATABASE bespoke_db
+
+-- CREATE TABLE `contact` (
+--   `id` Int( 5) AUTO_INCREMENT NOT NULL,
+--   `created_at` DATETIME NOT NULL,
+--   `name` VARCHAR( 255) NOT NULL,
+--   `email` VARCHAR( 50 ) NOT NULL,
+--   `phone` INT(15) NOT NULL,
+--   `message` VARCHAR(500) NOT NULL,
+  
+--   PRIMARY KEY ( `id` ) 
+-- );
